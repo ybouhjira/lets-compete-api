3f6404c713bd9e6f103c6a9d26ab826b65735d97
Youssef Bouhjira <b.youssef91@gmail.com>
2017-01-08 02:01:39 +0000

    Test: Login avec JWT

---
+++ tests/AppBundle/JWTAuthTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Test de la récupération du token JWT
+ */
+class JWTAuthTest extends WebTestCase
+{
+    public function test_auth_endpoint_returns_token()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/api/login_check', [
+                '_username' => 'foo', // from fixtures
+                '_password' => 'password',
+            ]
+        );
+
+        $res = $client->getResponse();
+        $c = $res->getContent();
+        $data = json_decode($res->getContent());
+
+        $this->assertEquals(200, $res->getStatusCode(), $c);
+        $this->assertObjectHasAttribute("token", $data, $c);
+    }
+}
