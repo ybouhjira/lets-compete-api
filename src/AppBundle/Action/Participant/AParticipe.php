f99ad5b58ec3f3b7d02e9d1049b20ea1d7fda21d
Youssef Bouhjira <b.youssef91@gmail.com>
2017-02-04 12:08:41 +0000

    Fix

--- src/AppBundle/Action/Participant/AParticipe.php
+++ src/AppBundle/Action/Participant/AParticipe.php
@@ -2,11 +2,11 @@
 
 namespace AppBundle\Action\Participant;
 
+use AppBundle\Entity\Competition;
+use AppBundle\Entity\Participant;
+use AppBundle\Entity\Participation;
 use Doctrine\ORM\EntityManager;
-use Proxies\__CG__\AppBundle\Entity\Competition;
-use Proxies\__CG__\AppBundle\Entity\Participant;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AParticipe
 {
@@ -17,13 +17,13 @@ class AParticipe
 
     public function __invoke($pid, $cid)
     {
+
         $result = $this->em
             ->createQueryBuilder()
             ->select('1')
-            ->from(Participant::class, 'p')
-            ->innerJoin(Competition::class, 'c')
-            ->where('c.id = :cid')
-            ->andWhere('p.id = :pid')
+            ->from(Participation::class, 'p')
+            ->where('p.participant = :pid')
+            ->andWhere('p.competition = :cid')
             ->setParameters(compact('pid', 'cid'))
             ->getQuery()
             ->getResult();
